c6e0b2343272227613bf9594d13a9f34eb4afec8
Gwidas <gwidas.hiroshi@icloud.com>
2022-04-18 10:04:52 +0000

    Added prints views and tempaltes

--- prints/views.py
+++ prints/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Print
 
-# Create your views here.
+
+def all_prints(request):
+    """ A view to show all prints, including sorting and search queries """
+
+    prints = Print.objects.all()
+
+    context = {
+        'prints': prints,
+    }
+
+    return render(request, 'prints/prints.html', context)

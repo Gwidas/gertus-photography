6a55d4e039b632b4de83243cc7de22edae0f80e3
Gwidas <gwidas.hiroshi@icloud.com>
2022-04-26 12:05:47 +0000

    updated fucntion bag_contents to  context.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,17 +1,40 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from prints.models import Print
 
 def bag_contents(request):
     """Counts itmes and total amount"""
 
     bag_items = []
     total = 0
-    product_count = 0
+    print_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        print_td = get_object_or_404(Print, pk=item_id)
+        total += quantity * print_td.price
+        print_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'print_td': print_td,
+        })
+
+    if total < settings.STANDART_DELIVERY_PRECENTAGE:
+        delivery = total * Decimal(settings.STANDART_DELIVERY_PRECENTAGE / 100)
+
+    else:
+        delivery = 0
+
+    grand_total = delivery + total
 
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'delivery': delivery,
+        'print_count': print_count,
+        'grand_total': grand_total,
     }
 
     return context

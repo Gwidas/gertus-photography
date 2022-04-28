6a4c9e5344e64a5d17d883ab4c4cf4fffb5d15d4
Gwidas <gwidas.hiroshi@icloud.com>
2022-04-28 17:08:55 +0000

    Added free delivery fucntion

--- bag/views.py
+++ bag/views.py
@@ -23,11 +23,11 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_size'][size] = quantity
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-
-    if item_id in list(bag.keys()):
-        bag[item_id] = quantity
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)

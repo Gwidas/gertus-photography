803c8b1515dc9832af2f7c74888a1d2ea48062d0
Gwidas <gwidas.hiroshi@icloud.com>
2022-05-03 10:41:20 +0000

    Added checkout view and tempaltes

--- gertus_photography/urls.py
+++ gertus_photography/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('prints/', include('prints.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
